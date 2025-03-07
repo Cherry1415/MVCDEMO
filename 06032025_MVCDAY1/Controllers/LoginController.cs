6a6b087177622b7c3a2fc20b9635fcb373df72a0
Cherry1415 <chiragpatelyo7@gmail.com>
2025-03-07 18:00:03 +0530

    07-03

---
+++ 06032025_MVCDAY1/Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _06032025_MVCDAY1.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult LogInPage()
+        {
+            return View();
+        }
+    }
+}
