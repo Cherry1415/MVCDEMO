d36ba046591295264029c6ed138fae4e4b8b11e3
Cherry1415 <chiragpatelyo7@gmail.com>
2025-03-15 12:58:47 +0530

    1503

---
+++ 06032025_MVCDAY1/Controllers/SellerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _06032025_MVCDAY1.Controllers
+{
+    public class SellerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SellerHome()
+        {
+            return View();
+        }
+    }
+}
---
+++ 06032025_MVCDAY1/Models/PaymentInitiateModel.cs
@@ -0,0 +1,12 @@
+﻿namespace _06032025_MVCDAY1.Models
+{
+    public class PaymentInitiateModel
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string contactNumber { get; set; }
+        public string address { get; set; }
+        public int amount { get; set; }
+    }
+}
+    
